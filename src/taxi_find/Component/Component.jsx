1547ec5341b08103e7058588bd40036af707024c
NishthaLath <lathnishtha775@gmail.com>
2024-11-16 04:53:54 +0900

    Accurately changed the taxi find page

--- src/taxi_find/Component/Component.jsx
+++ src/taxi_find/Component/Component.jsx
@@ -5,7 +5,7 @@ import CallButton from "../../share/CallButton.jsx";
 import BackButton from "../../share/BackButton.jsx";
 import "../../share/allshared.css"
 import React, {useState} from "react";
-
+import { playTextToSpeech } from "../../services/ttsService";
 
 export const Component = ({ className, ...props }) => {
 
@@ -13,23 +13,25 @@ export const Component = ({ className, ...props }) => {
     const [text, setText] = useState("");
 
     const handleGoBack = () => {
-        navigate(-1); // Navigate to the previous page
+        playTextToSpeech('이전 페이지로 돌아가겠습니다.');
+        navigate(-1); 
     };
 
     const handleNavigateToCall = () => {
-        navigate("/call"); // Navigate to the App.jsx in the 'call' directory
+        playTextToSpeech('안내원 도움 요청하기 버튼을 눌렀습니다. 안내원과 연결 중입니다. 잠시만 기다려주세요.');
+        navigate("/call");
     };
 
     const handleNavigateToFound = () => {
-        navigate("/taxi_found"); // Update this path if "App.jsx" has a different route, e.g., "/app"
+        playTextToSpeech('주위에 있는 택시를 찾았습니다. 기사님을 호출하고 싶으시면 확인 버튼을 눌러주세요.');
+        navigate("/taxi_found");
     };
 
     return (
         <div className={"div " + className}>
             <img
                 className="deagu-logo"
-                src="deagu-logo.png"
-                alt = ""
+                src="deagu-logo.png" alt="대구로고"
             />
             <BackButton
                 onClick={handleGoBack} label="뒤로가기"/>
@@ -41,17 +43,17 @@ export const Component = ({ className, ...props }) => {
 
             <div className="show-big-text">
                 <br/>
-                동대구역으로
+                주위에 있는
                 <br/>
-                가는 택시를
+                택시를
                 <br/>
-                찾아볼까요?
+                호출할까요?
             </div>
 
             <button className="div6"
                 onClick={handleNavigateToFound} style={{cursor: "pointer"}}> 예</button>
             <button className="div7" onClick={handleGoBack} style={{cursor: "pointer"}}>아니오</button>
-            <img className="image-13" src="deaguro.png" alt = ""/>
+            <img className="image-13" src="deaguro.png" alt="대구로고"/>
         </div>
     );
 };
