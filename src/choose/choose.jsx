237226c8db13f8e7db4ff0e90cf22d57d9a5b867
NishthaLath <lathnishtha775@gmail.com>
2024-11-16 01:25:36 +0900

    Added choose page to project

--- src/choose/choose.jsx
+++ src/choose/choose.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import CallButton from "../share/CallButton.jsx";
-import BackButton from "../share/BackButton";
+import BackButton from "../share/BackButton.jsx";
+import "./choose.css";
 import "../share/allshared.css";
+import { playTextToSpeech } from "../services/ttsService.js";
 
-export const choose = ({ className, ...props }) => {
+export const Choose = ({ className, ...props }) => {
     const navigate = useNavigate();
 
     const handleGoBack = () => {
@@ -16,29 +18,38 @@ export const choose = ({ className, ...props }) => {
     };
 
     const handleNavigateToCall = () => {
+        playTextToSpeech("안내원 도움 요청하기 버튼을 눌렀습니다. 안내원이 도움을 드리러 오겠습니다.", 1.0);
         navigate("/call");
     }
 
+    // test용 임시 함수 생성
+    const handleNavigateToFindingTaxi = () => {
+        navigate("/taxi_find");
+    }
 
     return (
-        <div className={"main " + className}>
+        <div className={"choose " + className}>
             <img
                 className="deagu-logo"
                 src="deagu-logo.png" alt = "대구로고"
             />
             <div className="show-big-text">
-                도착지 약도
+                이동수단을
                 <br/>
-                출력 서비스를
-                <br/>
-                시작하겠습니다.{" "}
+                선택해주세요.
             </div>
-            <div className="depth-3-frame-1">
-                <br/>
+            <div className="frame-1">
                 <button onClick={handleNavigate} className="navigate-button">
-                    시작하기
+                    대중교통
                 </button>
             </div>
+
+            <div className="frame-2">
+                <button onClick={handleNavigateToFindingTaxi} className="navigate-button">
+                    택시
+                </button>
+            </div>
+
             <div className="call-button-frame">
                 <CallButton onClick={handleNavigateToCall}/>
             </div>
@@ -50,4 +61,4 @@ export const choose = ({ className, ...props }) => {
     );
 };
 
-export default choose;
+export default Choose;
