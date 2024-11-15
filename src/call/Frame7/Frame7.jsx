636e4e7a258e61593ec07ea1d433e6ec632306a0
NishthaLath <lathnishtha775@gmail.com>
2024-11-16 01:03:27 +0900

    Changed ui of call page

--- src/call/Frame7/Frame7.jsx
+++ src/call/Frame7/Frame7.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Frame7.css";
 import BackButton from "../../share/BackButton.jsx";
+import "../../share/allshared.css";
 import { playTextToSpeech } from "../../services/ttsService";
 
 export const Frame7 = ({ className, ...props }) => {
@@ -13,24 +14,23 @@ export const Frame7 = ({ className, ...props }) => {
     };
 
     return (
-      <div className={"frame-7 " + className}>
-          <BackButton
-              onClick={handleNavigateToMain} label="처음으로"/>
-          <img
-              className="image-removebg-preview-19-2"
-              src="image-removebg-preview-19-20.png"
-              alt = ""/>
-          <div className="div2">
-              담당 정류장 안내원과
-              <br/>
-              전화연결 중입니다.
-              <br/>
-              잠시만 기다려주세요.{" "}
-          </div>
-          <div className="line-3"></div>
-          <img className="image-10" src="image-100.png" alt = ""/>
-      </div>
-  );
+        <div className={"frame-7 " + className}>
+            <BackButton
+                onClick={handleNavigateToMain} label="처음으로"/>
+            <img
+                className="deagu-logo"
+                src="deagu-logo.png" alt = ""/>
+            <div className="line-upper"></div>
+            <div className="show-big-text">
+                담당 정류장 안내원과
+                <br/>
+                전화연결 중입니다.
+                <br/>
+                잠시만 기다려주세요.{" "}
+            </div>
+            <img className="image-10" src="image-100.png" alt = ""/>
+        </div>
+    );
 };
 
 export default Frame7;
